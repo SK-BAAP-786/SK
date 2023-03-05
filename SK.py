e0c0ff2df7c0482ba74ead3452cf1da9e88db6a4
SK BAAP G <111557947+skbaap@users.noreply.github.com>
2023-03-05 22:02:58 +0530

    Update SK.py

--- SK.py
+++ SK.py
@@ -1,97 +1,99 @@
+import os, platform
 
-
- import os, platform
- 
 try:
- 
+
     import requests
- 
+
 except:
- 
+
     os.system('pip install requests')
- 
+
 import requests
- 
+
 bit = platform.architecture()[0]
- 
+
 if bit == '64bit':
- 
+
     from SK_enc import Main
- 
-    Main()
- 
-elif bit == '32bit':
- 
-    from SKK32 import Main
- 
-    Main()
-
-
-
- 
-
-
-
-
-
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
-
- 
-
-
-
- 
-
-
-
- 
-
-
-
- 
-
-
-
- 
-
-
-
- 
 
     Main()
-
- 
 
 elif bit == '32bit':
 
- 
-
     from SKK32 import Main
 
+    Main()
+
+ 
  
 
-    Main()
+
+
+
+ 
+
+
+
+
+
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
